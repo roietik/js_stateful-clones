ed9a58b85220165da088da195bac73484fe92ad0
Radek <radoslaw.grzymala@hotmail.com>
2025-03-14 19:22:51 +0100

    Feat: transform states with clones

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,32 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const ACTIONS = { addProperties, removeProperties, clear };
+  const history = [];
+  let currentState = { ...state };
+
+  for (const action of actions) {
+    currentState = ACTIONS[action.type](currentState, action);
+    history.push({ ...currentState });
+  }
+
+  return history;
+}
+
+function addProperties(state, action) {
+  return { ...state, ...action.extraData };
+}
+
+function removeProperties(state, action) {
+  const newState = { ...state };
+
+  action.keysToRemove.forEach((key) => delete newState[key]);
+
+  return newState;
+}
+
+function clear(state) {
+  return {};
 }
 
 module.exports = transformStateWithClones;
